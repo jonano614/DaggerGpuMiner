c81bb657955970eaaf3d82b3dda03693cb0ebf48
Evgeniy <jonano614@mail.ru>
2018-01-26 11:18:00 +0300

    Alfa-version 0.0.1

---
+++ GpuMiner/version.h
@@ -0,0 +1,27 @@
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+#define VERSION_MAJOR               0
+#define VERSION_MINOR               0
+#define VERSION_REVISION            1
+
+#define VER_FILE_DESCRIPTION_STR    "Standalone GPU/CPU miner for Dagger coin"
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
+#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+
+#define VER_PRODUCTNAME_STR         "Dagger GPU Miner"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
+#define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
+#define VER_COPYRIGHT_STR           ""
+
+#ifdef _DEBUG
+#define VER_VER_DEBUG             VS_FF_DEBUG
+#else
+#define VER_VER_DEBUG             0
+#endif
+
+#define VER_FILEFLAGS               VER_VER_DEBUG
