a26d41355644b8fb5d0ae3183fe24a9ab7008b32
Jonano <jonano614@mail.ru>
2018-02-04 00:00:32 +0300

    Version 0.2.1

--- GpuMiner/version.h
+++ GpuMiner/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               2
-#define VERSION_REVISION            0
+#define VERSION_REVISION            1
 
 #define VER_FILE_DESCRIPTION_STR    "Standalone GPU/CPU miner for Dagger coin"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
